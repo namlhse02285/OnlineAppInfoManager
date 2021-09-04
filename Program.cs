5de3ad8f2544ff5da81ccef7b4255b70b2cbe91d
nam-asus <namlhse02285@gmail.com>
2021-09-04 10:48:34 +0900

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OnlineAppInfoManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
