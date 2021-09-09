b9570befdc7b8a670057d9973d78b1e844225a61
nam-asus <namlhse02285@gmail.com>
2021-09-09 10:44:10 +0900

    Add blank SakuraIroDreamer game info

---
+++ GameInfo/SakuraIroDreamer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineAppInfoManager.GameInfo
+{
+    class SakuraIroDreamer
+    {
+        //[SAKURA_IRO_DREAMER_START][SAKURA_IRO_DREAMER_END]
+    }
+}
