c52509005e19ab9fd785b8bfa53be31a620a6dd2
nam-asus <namlhse02285@gmail.com>
2021-09-20 16:40:16 +0900

    add common url

---
+++ Common/OnlineCommonUrl.cs
@@ -0,0 +1,7 @@
+﻿namespace OnlineAppInfoManager.Common
+{
+    class OnlineCommonUrl
+    {
+        //[COMMON_INFO_START]eyJkaXNjb3JkX2hvc2hpIjoiaHR0cHM6Ly9kaXNjb3JkLmNvbS9pbnZpdGUvZ1pmRnpRM3oiLCJ2aXN1bF9ub3ZlbF9ncm91cF9mYiI6Imh0dHBzOi8vd3d3LmZhY2Vib29rLmNvbS9ncm91cHMvdmlzdWFsbm92ZWx2aWV0IiwiaG9zaGlfZmJfZmFucGFnZSI6Imh0dHBzOi8vd3d3LmZhY2Vib29rLmNvbS9Ib3NoaUZhbnN1YiIsInZpc3VsX25vdmVsX2ZhbnBhZ2UiOiJodHRwczovL3d3dy5mYWNlYm9vay5jb20vdmlzdWFsbm92ZWx2aWV0Iiwid2Vic2l0ZV9ob3NoaSI6Imh0dHA6Ly93d3cuaG9zaGl2c3ViLmNvbSJ9[COMMON_INFO_END]
+    }
+}
