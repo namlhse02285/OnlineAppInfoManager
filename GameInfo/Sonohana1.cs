eb1eb9822c8bf992d3d1e2023827ba985bc87c4e
nam-asus <namlhse02285@gmail.com>
2021-11-20 20:46:16 +0900

    Add Sonohana1

---
+++ GameInfo/Sonohana1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineAppInfoManager.GameInfo
+{
+    class Sonohana1
+    {
+        //[GAME_INFO_START]eyJhdXRvX3VwZGF0ZV9maWxlcyI6W3siZmlsZV9wYXRoIjoiZ2FtZS90bC9WaWV0bmFtZXNlL2ltYWdlcy9jcmVkaXRzL2NyZWRpdHMwMi5wbmciLCJ1cmwiOiJodHRwczovL2Nkbi5kaXNjb3JkYXBwLmNvbS9hdHRhY2htZW50cy85MTE1ODI2ODE0NTQ3NDM1NjIvOTExNTgyODI1MjM1NDk2OTgwL2NyZWRpdHMwMi5wbmciLCJoYXNoIjoiYTAwMDgwNGFmN2EwMGMxYTNmYjY3ZjQ4NzJlMzVkNzQifV19[GAME_INFO_END]
+    }
+}
